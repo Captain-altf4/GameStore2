54516c7315608a38467feb91192d3e5a62fcc73c
Captain-altf4 <leealex.2006@mail.ru>
2022-05-02 18:36:37 +0700

    added WindowBorder

---
+++ GameStore2/Custom Controlls/WindowBorder.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace GameStore2.Custom_Controlls
+{
+    public partial class WindowBorder : UserControl
+    {
+        Window parent;
+        public WindowBorder(Window parent)
+        {
+            this.parent = parent;
+            InitializeComponent();
+        }
+
+        public WindowBorder()
+        {
+            InitializeComponent();
+        }
+
+        private void b_Close_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.parent.Close();
+        }
+
+        private void b_Minimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.parent.WindowState = WindowState.Minimized;
+        }
+
+        private void UserControl_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                parent.DragMove();
+        }
+    }
+}
