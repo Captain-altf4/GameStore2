55828a9e23afb7926d312dd163581c7c19e0ffd1
Ahead1Face <j5p7l@mail.ru>
2022-05-14 15:11:20 +0700

    added LoginData

---
+++ GameStore2/LoginData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace GameStore2
+{
+    class LoginData
+    {
+        public struct CurrentUser
+        {
+            public int Id { get; set; }
+            public string Login { get; set; }
+            public string Mail { get; set; }
+
+            public static bool CheckLogin(string userLogin)
+            {
+                Regex rx = new Regex(@"^[a-zA-Z0-9-_]{3,32}$");
+                return rx.IsMatch(userLogin);
+            }
+
+            public static bool CheckMail(string userMail)
+            {
+                Regex rx = new Regex(@"^((([0-9A-z]{1}[-0-9A-z\.]{0,30}[0-9A-z]?)|([0-9A-zА-я]{1}[-0-9A-zА-я\.]{0,30}[0-9A-zА-я]?))@([-A-Za-z]{1,}\.){1,}[-A-Za-z]{2,})$");
+                return rx.IsMatch(userMail);
+            }
+
+            public static bool CheckPassword(string userPassword)
+            {
+                Regex rx = new Regex(@"^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9A-z!@#$%^&*]{6,}");
+                return rx.IsMatch(userPassword);
+            }
+        }
+    }
+}
